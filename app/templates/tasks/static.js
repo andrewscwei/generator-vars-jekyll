d5682d33fc7a3ff6f564aa8ab1be7bdda0caa842
andrewscwei <andrewscwei@gmail.com>
2015-07-07 17:26:18 -0700

    Fix static.js

--- app/templates/tasks/static.js
+++ app/templates/tasks/static.js
@@ -23,14 +23,14 @@ gulp.task('static', ['images', 'videos', 'fonts', 'styles', 'scripts', 'extras']
     return merge
     (
         gulp.src([config.paths.tmp+'/**/*.'+config.patterns.extras])
-            .pipe(gulp.dest(config.paths.build+'')),
+            .pipe(gulp.dest(config.paths.build)),
         gulp.src([config.paths.tmp+'/assets/**/*.'+config.patterns.images, config.paths.tmp+'/assets/**/*.'+config.patterns.videos, config.paths.tmp+'/assets/**/*.'+config.patterns.fonts, config.paths.tmp+'/assets/**/*.'+config.patterns.styles, config.paths.tmp+'/assets/**/*.'+config.patterns.scripts])
             .pipe($if(!config.env.skipRev, $rev()))
             .pipe(gulp.dest(config.paths.build+'/assets'))
             .pipe($size({ title: 'build', gzip: true }))
             .pipe($if(!config.env.skipRev, $rev.manifest()))
-            .pipe($if(!config.env.skipRev, gulp.dest(config.paths.tmp+''))),
+            .pipe($if(!config.env.skipRev, gulp.dest(config.paths.tmp))),
         gulp.src([config.paths.tmp+'/*.'+config.patterns.images, config.paths.tmp+'/**/*.'+config.paths.sourcemaps])
-            .pipe(gulp.dest(config.paths.build+''))
+            .pipe(gulp.dest(config.paths.build))
     );
 });
